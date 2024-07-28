cfb21bdea6a1e66a1101d8ba87ee3b17ffaebfe3
Daniel Garcia Rojas <d.g.r.developer@gmail.com>
2024-07-28 17:25:02 -0400

    front login y signup

--- components/modal-video.tsx
+++ components/modal-video.tsx
@@ -76,9 +76,14 @@ export default function ModalVideo({
             leaveFrom="oopacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="max-w-6xl mx-auto h-full flex items-center">
+            <div className="w-full max-w-6xl mx-auto h-full flex items-center">
               <Dialog.Panel className="w-full max-h-full aspect-video bg-black overflow-hidden">
-                <video ref={videoRef} width={videoWidth} height={videoHeight} loop controls>
+                <video ref={videoRef}
+                       className="w-full h-full"
+                       width={videoWidth} 
+                       height={videoHeight} 
+                       loop 
+                       controls>
                   <source src={video} type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
